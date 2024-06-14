ed38f2baba4e14a18e83f0fda067cfa3bc24d25c
ABHISHEK BAGEWADI <abhishek.bagewadi@rmb.co.za>
2024-06-14 15:54:21 +0530

    adding updated files

--- com/servlets/MyServlet.java
+++ com/servlets/MyServlet.java
@@ -9,26 +9,48 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 //@WebServlet(urlPatterns ="/myservlet", name = "MyServlet")
+//@WebServlet(name = "MyServlet", value = "/myservlet")
 
+//@WebServlet
 public class MyServlet extends HttpServlet {
     /*public MyServlet() {
         super();
     }*/
 
+//    private String message;
+//
+//    public void init() throws ServletException {
+//        // Do required initialization
+//        message = "Hello World";
+//    }
+//
+//    public void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//
+//        // Set response content type
+//        response.setContentType("text/html");
+//
+//        // Actual logic goes here.
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>" + message + "</h1>");
+//    }
+//
+//    public void destroy() {
+//        // do nothing.
+//    }
+
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("this is get method....");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h1>This is get method of my servlet</h1>");
     }
 
-    /*@Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
-        System.out.println("this is get method....");
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<h1>This is get method of my servlet</h1>");
-    }*/
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+
 }
--- main/webapp/index.jsp
+++ main/webapp/index.jsp
@@ -4,6 +4,6 @@
 </body>
 <p>This is servlet testing</p>
 <br>
-<a href ="login">Login Servlet</a>
+<a href ="MyServlet">Login Servlet</a>
 
 </html>
